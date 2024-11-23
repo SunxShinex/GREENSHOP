5fa265ac5f9bd9b55ff6f8186489fcbfd825284c
Well As <goodfilled@gmail.com>
2024-11-23 17:37:34 +0100

    some fix

--- js/scripts.js
+++ js/scripts.js
@@ -16,19 +16,52 @@ $( function() {
     $( "#accordion" ).accordion();
   } );
 
-
-
-
-  var menuToggle = document.querySelector('[data-js="menu-toggle"]');
-
-  menuToggle.addEventListener('click', function () {
-      document.body.classList.toggle('panel-open');
-      menuToggle.classList.toggle('open');
+  const rangeInput = document.querySelectorAll(".range-input input"),
+  priceInput = document.querySelectorAll(".price-input input"),
+  range = document.querySelector(".slider .progress");
+  let priceGap = 1000;
+  
+  priceInput.forEach(input =>{
+      input.addEventListener("input", e =>{
+          let minPrice = parseInt(priceInput[0].value),
+          maxPrice = parseInt(priceInput[1].value);
+          
+          if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+              if(e.target.className === "input-min"){
+                  rangeInput[0].value = minPrice;
+                  range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+              }else{
+                  rangeInput[1].value = maxPrice;
+                  range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+              }
+          }
+      });
   });
   
-  var closePanel = document.querySelector('[data-js="hidden-panel-close"]');
+  rangeInput.forEach(input =>{
+      input.addEventListener("input", e =>{
+          let minVal = parseInt(rangeInput[0].value),
+          maxVal = parseInt(rangeInput[1].value);
   
-  closePanel.addEventListener('click', function () {
-      document.body.classList.remove('panel-open');
-      menuToggle.classList.remove('open');
+          if((maxVal - minVal) < priceGap){
+              if(e.target.className === "range-min"){
+                  rangeInput[0].value = maxVal - priceGap
+              }else{
+                  rangeInput[1].value = minVal + priceGap;
+              }
+          }else{
+              priceInput[0].value = minVal;
+              priceInput[1].value = maxVal;
+              range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+              range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+          }
+      });
+  });
+
+  document.querySelectorAll('.input-min, .input-max').forEach(input => {
+    input.style.width = input.value.length + 1 + "ch";
+  
+    input.addEventListener('input', function () {
+      this.style.width = this.value.length + 1 + "ch";
+    });
   });
